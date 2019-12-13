2a5ea8a21b9839ce1ffc8298fd836eafa10db0a6
Jimzjy <jimzjy@gmail.com>
2019-12-13 17:25:02 +0800

    update

--- src/mqtt/mqtt.module.ts
+++ src/mqtt/mqtt.module.ts
@@ -4,10 +4,17 @@ import { ClientsModule } from '@nestjs/microservices';
 import { MqttController } from './mqtt.controller'
 import { MQTT_SERVICE } from './mqtt.constants'
 import { MqttService } from './mqtt.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DeviceModule } from '../device/device.module';
 
 @Module({
   imports: [
     ClientsModule.register([{ name: MQTT_SERVICE, transport: Transport.MQTT }]),
+    MongooseModule.forRoot('mongodb://localhost/jff', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    DeviceModule,
   ],
   controllers: [MqttController],
   providers: [MqttService],

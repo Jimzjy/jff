8c910081df653971ef7429a75a634e4579483577
Jimzjy <jimzjy@gmail.com>
2020-05-13 01:07:57 +0800

    bug fix

--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -43,7 +43,6 @@ export class MqttService {
   }
 
   transferCommand(id: string, command: string, data?: string) {
-    console.log(id, command, data)
     this.client.send(`/${id}/${command}`, data ? data : '').toPromise()
   }
 }
